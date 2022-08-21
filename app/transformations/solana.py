dfa68ae92a2faee4d9529ab7d10cd27e60650044
kavisek <kavi.skhon@gmail.com>
2022-08-21 16:14:10 -0700

    feat: adding transformation skeleton and kaggle script

--- app/transformations/solana.py
+++ app/transformations/solana.py
@@ -5,7 +5,39 @@ class SolanaJobs:
     def __init__(self):
         pass
 
-    def generate_dataframe(self) -> pd.DataFrame:
-        df = pd.DataFrame()
-
+    def solana_dataframe(self) -> pd.DataFrame:
+        """Import Dataset"""
+        df = pd.read_csv(
+            "./datasets/psycon/solana-usdt-to-20220-4-historical-data/sol-usdt.csv"
+        )
         return df
+
+    # DataTypes
+    # Rename Columns
+    # Create Columns
+    # Drop Columns
+    # Filter by Date
+    # Filter by string
+    # Filter by Regex
+    # Filter by Multilple conditions
+
+    # Descriptive Statistics
+    # Counts
+    # Describe
+    # GroupBy
+    # GroupBy Custom Aggregations
+
+    # Rank By
+    # Rank By Window
+
+    # Fill Nulls
+    # Remove Nulls
+
+    # Joins/Merges
+    # Pivots
+    # Split and Explodes
+
+    # Writing Data to CSV
+    # Writing Data to Excel
+    # Wriing Data to JSON
+    # Writing Data to Parquet
