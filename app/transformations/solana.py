91a0ad33e69ecd8b7b93cd742bba3b66a75e8ab6
kavisek <kavi.skhon@gmail.com>
2022-08-21 15:37:23 -0700

    feat: adding project structure

---
+++ app/transformations/solana.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+class SolanaJobs:
+    def __init__(self):
+        pass
+
+    def generate_dataframe(self) -> pd.DataFrame:
+        df = pd.DataFrame()
+
+        return df
